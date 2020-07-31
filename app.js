f9d5eac78e28f36e9543a0d0471531e0e5654dd9
Inlic <bwiedeback@gmail.com>
2020-07-31 01:25:12 -0600

    Added features cat purge, gone message.

--- app.js
+++ app.js
@@ -12,25 +12,90 @@ let kittens = [];
  * then add that data to the kittens list.
  * Then reset the form
  */
-function addKitten(event) {}
+function addKitten(event) {
+  event.preventDefault();
+  if(document.getElementById("welcome") !== null){
+    document.getElementById("welcome").remove();
+  }
+  let form = event.target;
+  let catId = generateId();
+  let catName = form.name.value;
+  let catMood = "tolerant";
+  let catAffection = 5;
+  let newKitten = {id: catId, name: catName, mood: catMood, affection: catAffection}
+  let duplicate = false; 
+  for(let i = 0; i < kittens.length; i++){
+    if(newKitten.name === kittens[i].name){
+      alert("Kitten cannot have the same name as an existing kitten.");
+      duplicate = true;
+    }  
+  }
+  if(!duplicate){
+  kittens.push(newKitten);
+  saveKittens();
+  form.reset();
+  }
+  saveKittens();
+  form.reset();
+}
 
 /**
  * Converts the kittens array to a JSON string then
  * Saves the string to localstorage at the key kittens
  */
-function saveKittens() {}
+function saveKittens() {
+  window.localStorage.setItem("kittens",JSON.stringify(kittens));
+  drawKittens();
+}
 
 /**
  * Attempts to retrieve the kittens string from localstorage
  * then parses the JSON string into an array. Finally sets
  * the kittens array to the retrieved array
  */
-function loadKittens() {}
+function loadKittens() {
+  let kittensData = JSON.parse(window.localStorage.getItem("kittens"))
+  if(kittensData){
+    kittens = kittensData
+    document.getElementById("purge").classList.remove("hidden");
+  };
+}
 
 /**
  * Draw all of the kittens to the kittens element
  */
-function drawKittens() {}
+function drawKittens() {
+  let template ="";
+  kittens.forEach(kitten =>{
+    template+=`
+    <div class="card shadow bg-dark m-2 container">
+      <div id =${kitten.id} class ="container d-flex justify-content-center kitten ${kitten.mood}">  
+        <img src="https://robohash.org/${kitten.name}.png?set=set4">
+      </div>
+      <div class = "text-danger ${kitten.affection > 0 ? 'hidden': ''}">Woe to you mortal!  Doom has come to your kitten, ${kitten.name}!</div>
+      <div class = "d-flex ${kitten.affection === 0 ? 'hidden' : ''}">
+        <div class="container d-flex space-around">
+          <label class="text-light" for="name">Name:</label>
+          <h5 id="${kitten.id}-name" class="text-light " name="name">${kitten.name}</h5>
+          
+          <label class="text-light" for="mood">Mood:</label>
+          <h5 id="${kitten.id}-mood" class="text-light" name="mood">${kitten.mood}</h5>
+          
+          <label class="text-light" for="affection">Affection:</label>
+          <h5 id="${kitten.id}-affection" class="text-light" name="affection">${kitten.affection}</h5>
+          
+        </div>
+        <br>
+        <div class="container d-flex space-around">
+          <button class="btn-cancel" onclick="pet('${kitten.id}')">Pet</button>
+          <button class="button btn-green" onclick="catnip('${kitten.id}')">Catnip</button>
+        </div>
+      </div> 
+    </div>
+    `
+    document.getElementById("kittens").innerHTML = template;
+  })
+}
 
 /**
  * Find the kitten in the array by its id
@@ -38,7 +103,7 @@ function drawKittens() {}
  * @return {Kitten}
  */
 function findKittenById(id) {
-  return kittens.find(k => k.id == id);
+  return kittens.find(kitten => kitten.id === id);
 }
 
 /**
@@ -50,7 +115,21 @@ function findKittenById(id) {
  * save the kittens
  * @param {string} id
  */
-function pet(id) {}
+function pet(id) {
+  let index = kittens.findIndex(kitten => kitten.id === id);
+  let petCheck = Math.random();
+  console.log(petCheck);
+  if(kittens[index].affection > 0){ 
+    petCheck > .7 ? kittens[index].affection += 1 : kittens[index].affection -= 1;
+    if(kittens[index].affection === 0){
+      console.log("Runaway little girl!");
+      setKittenMood(findKittenById(id));
+      //TODO runaway state add runaway elements to HTML, change text, change picture, remove buttons
+    } 
+  }
+  setKittenMood(findKittenById(id));
+  saveKittens();
+}
 
 /**
  * Find the kitten in the array of kittens
@@ -59,18 +138,49 @@ function pet(id) {}
  * save the kittens
  * @param {string} id
  */
-function catnip(id) {}
+function catnip(id) {
+  let index = kittens.findIndex(kitten => kitten.id === id);
+  kittens[index].mood = "tolerant";
+  kittens[index].affection = 5
+  setKittenMood(findKittenById(id));
+  saveKittens();
+}
 
 /**
  * Sets the kittens mood based on its affection
  * Happy > 6, Tolerant <= 5, Angry <= 3, Gone <= 0
  * @param {Kitten} kitten
  */
-function setKittenMood(kitten) {}
+function setKittenMood(kitten) {
+  console.log(kitten);
+  let kittenAffection = kitten.affection
+  switch(true){
+    case kittenAffection === 0:
+      kitten.mood = "gone"
+      break;
+    case kittenAffection <= 3:
+      kitten.mood = "angry"
+      break;
+    case kittenAffection <= 5:
+      kitten.mood = "tolerant"
+      break;
+    case kittenAffection > 6:
+      kitten.mood = "happy"
+      break;
+  }
+}
 
 function getStarted() {
   document.getElementById("welcome").remove();
   drawKittens();
+}
+
+function purgeKittens(){
+  document.getElementById("purge").remove();
+  localStorage.removeItem("kittens");
+  kittens = []
+  loadKittens();
+  saveKittens();
 }
 
 /**
@@ -90,3 +200,6 @@ function generateId() {
     Math.floor(Math.random() * 10000000)
   );
 }
+
+loadKittens();
+
